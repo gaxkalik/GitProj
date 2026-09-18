cef7fb16310e03ca9621ed40967f31cebf5ad729
gaxkalik <gaxkalik@gmail.com>
2026-09-18 12:45:11 +0400

    ad

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         // A simple wrapper that does not extend Application
         // This prevents the Java launcher from aggressively verifying JavaFX modules on the module path.
         CalculatorApp.main(args);
-        System.out.println("hel");
+        System.out.println("hello !!!!!");
     }
 }
